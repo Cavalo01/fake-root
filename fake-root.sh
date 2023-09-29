1aa5eb6a2678aca60f9f560b4d326237e2283aa5
Cavalo01 <124747668+Cavalo01@users.noreply.github.com>
2023-09-29 10:46:08 -0300

    fake-root.sh

--- fake-root.sh
+++ fake-root.sh
@@ -2,13 +2,16 @@ function whoami() {
   echo "root"
 }
 
+
 function su() {
   :
 }
 
+
 function ls() {
-  /bin/ls "$@" 2>/dev/null || echo -e "apex  dev   lib32         mnt      root  system  vendor\nbin   etc   lib64         odm      run   tmp\nboot  home  linkerconfig  proc     sbin  usr\ndata  lib   media         product  sys   var"
+  /bin/ls "$@"
 }
+
 
 function sudo() {
   local result
@@ -16,4 +19,18 @@ function sudo() {
   echo "$result"
 }
 
-./fake-root.sh
+# Agora, você pode chamar as funções personalizadas
+whoami_result=$(whoami)
+echo "Testando whoami: $whoami_result"
+
+su_result=$(su)
+echo "Testando su: $su_result"
+
+ls_result=$(ls)
+echo "Testando ls: $ls_result"
+
+sudo_result=$(sudo ls)
+echo "Testando sudo (rodando ls): $sudo_result"
+
+echo "Fake Root Ativado! Contato: t.me/RGB_01"
+echo "Fake Root Activated! Contact: t.me/RGB_01"
