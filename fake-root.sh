d1e6442a58911c5357c41becbcdf598b14a9497d
Cavalo01 <124747668+Cavalo01@users.noreply.github.com>
2023-09-29 11:18:03 -0300

    fake-root.sh

--- fake-root.sh
+++ fake-root.sh
@@ -1,3 +1,5 @@
+g='temp.txt'
+
 function whoami() {
   echo "root"
 }
@@ -9,7 +11,7 @@ function su() {
 
 
 function ls() {
-  /bin/ls "$@"
+  /bin/ls "$@" 2>/dev/null || echo -e "apex  dev   lib32         mnt      root  system  vendor\nbin   etc   lib64         odm      run   tmp\nboot  home  linkerconfig  proc     sbin  usr\ndata  lib   media         product  sys   var"
 }
 
 
@@ -19,18 +21,44 @@ function sudo() {
   echo "$result"
 }
 
-# Agora, você pode chamar as funções personalizadas
+function reboot() {
+  sleep 10
+}
+
+function shutdown() {
+  while true; do
+  sleep 20
+  done
+}
+
+function poweroff() {
+  while true; do
+  sleep 20
+  done
+}
+
 whoami_result=$(whoami)
-echo "Testando whoami: $whoami_result"
+echo "Testando whoami: $whoami_result" >> $g
 
 su_result=$(su)
-echo "Testando su: $su_result"
+echo "Testando su: $su_result" >> $g
 
 ls_result=$(ls)
-echo "Testando ls: $ls_result"
+echo "Testando ls: $ls_result" >> $g
 
 sudo_result=$(sudo ls)
-echo "Testando sudo (rodando ls): $sudo_result"
+echo "Testando sudo (rodando ls): $sudo_result" >> $g
+
+reboot_result=$(reboot)
+echo "Testando reboot: $reboot_result" >> $g
+
+shutdown_result=$(shutdown)
+echo "Testando shutdown: $shutdown_result" >> $g
+
+poweroff_result=$(poweroff)
+echo "Testando powerorf: $poweroff_result" >> $g
+
+rm $g
 
 echo "Fake Root Ativado! Contato: t.me/RGB_01"
 echo "Fake Root Activated! Contact: t.me/RGB_01"
