ecc92dfd851e63ac0d5cfa2a9fd61e83fb7c7849
Cavalo01 <124747668+Cavalo01@users.noreply.github.com>
2023-09-29 02:10:39 -0300

    fake-root.sh

--- fake-root.sh
+++ fake-root.sh
@@ -1,9 +1,19 @@
-alias whoami='echo root'
-alias su='echo'
-alias sudo='echo'
-alias ls='custom_ls'
-custom_ls() {
-    /bin/ls "$@" 2>/dev/null || echo -e "apex  dev   lib32         mnt      root  system  vendor\nbin   etc   lib64         odm      run   tmp\nboot  home  linkerconfig  proc     sbin  usr\ndata  lib   media         product  sys   var"
+whoami() {
+  echo "root"
 }
+
+su() {
+  :
+}
+
+ls() {
+  /bin/ls "$@" 2>/dev/null || echo -e "apex  dev   lib32         mnt      root  system  vendor\nbin   etc   lib64         odm      run   tmp\nboot  home  linkerconfig  proc     sbin  usr\ndata  lib   media         product  sys   var"
+}
+
+sudo() {
+  result="$(eval $@)"
+  echo "$result"
+}
+
 echo "Fake-Root Ativado! Contato: t.me/RGB_01"
 echo "Fake-Root Activated! Contact: t.me/RGB_01"
